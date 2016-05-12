2a840889c82d943b879d56393548b3c9a76fce8b
Daniel Fleiderman <dfleider@MacBook-Pro-de-Daniel-4.local>
2016-05-12 13:55:13 -0300

    leyendo json instagram

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,4 +1,19 @@
 class InstagramController < ApplicationController
+skip_before_action :verify_authenticity_token
+
+require 'rest-client'
+require 'json'
+
   def inicio
+
+  	tag = params[:tag].to_s
+  	#recibo el parametro del tag a buscar
+  	busqueda = RestClient.get 'https://api.instagram.com/v1/tags/'+tag+'/media/recent?access_token=3230931781.5fe4edf.ca4155e5c64647dfbfacc7d9ef64bd35'
+  	#https://api.instagram.com/oauth/authorize/?client_id=5fe4edf794844d62b62b81bbb07d6f0a&redirect_uri=http://tarea-deployment.herokuapp.com/&response_type=token&scope=public_content
+  	busquedaParseada = JSON.parse busqueda
+  	puts busquedaParseada["data"][0]["likes"]["count"]
+  	puts busquedaParseada["data"][1]["likes"]
+  	puts busquedaParseada["data"][2]["caption"]["from"]["username"]
+  	puts busquedaParseada["data"][3]["caption"]["from"]["username"]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root 'instagram#inicio'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+     post 'instagram/tag/buscar' => 'instagram#inicio'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
