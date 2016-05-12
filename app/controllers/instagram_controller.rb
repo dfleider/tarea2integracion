033c987a04fc90a3d59f9da79d9accff2346923d
Daniel Fleiderman <dfleider@MacBook-Pro-de-Daniel-4.local>
2016-05-12 16:34:13 -0300

    busqueda completa, falta hacer el json para respuesta

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -7,13 +7,24 @@ require 'json'
   def inicio
 
   	tag = params[:tag].to_s
+  	token = params[:access_token].to_s
   	#recibo el parametro del tag a buscar
-  	busqueda = RestClient.get 'https://api.instagram.com/v1/tags/'+tag+'/media/recent?access_token=3230931781.5fe4edf.ca4155e5c64647dfbfacc7d9ef64bd35'
+  	busqueda = RestClient.get 'https://api.instagram.com/v1/tags/'+tag+'/media/recent?access_token='+token
+
+  	busquedaTotal = RestClient.get 'https://api.instagram.com/v1/tags/'+tag+'?access_token='+token
   	#https://api.instagram.com/oauth/authorize/?client_id=5fe4edf794844d62b62b81bbb07d6f0a&redirect_uri=http://tarea-deployment.herokuapp.com/&response_type=token&scope=public_content
   	busquedaParseada = JSON.parse busqueda
-  	puts busquedaParseada["data"][0]["likes"]["count"]
-  	puts busquedaParseada["data"][1]["likes"]
-  	puts busquedaParseada["data"][2]["caption"]["from"]["username"]
-  	puts busquedaParseada["data"][3]["caption"]["from"]["username"]
+  	total = JSON.parse busquedaTotal
+  	
+  	for i in 0..19 
+  	puts busquedaParseada["data"][i]["tags"]
+  	puts busquedaParseada["data"][i]["caption"]["from"]["username"]
+  	puts busquedaParseada["data"][i]["likes"]["count"]
+  	puts busquedaParseada["data"][i]["images"]["standard_resolution"]["url"]
+  	puts busquedaParseada["data"][i]["caption"]["text"]
+  	puts "------------------------------------------------------------------"
+
+  end
+    puts 'cantidadTag ='+total["data"]["media_count"].to_s
   end
 end
