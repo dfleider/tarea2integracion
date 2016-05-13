9c851166679f123c7077638bf1d8e2f3b637f3fb
Daniel Fleiderman <dfleider@MacBook-Pro-de-Daniel-4.local>
2016-05-13 18:48:04 -0300

    armando deploy listo?

--- test/testunitarios.rb
+++ test/testunitarios.rb
@@ -1,3 +1,9 @@
-#require 'coveralls'
-#Coveralls.wear!
+require 'test_helper'
 
+class InstagramControllerTest < ActionController::TestCase
+  test "should get inicio" do
+    get :inicio
+    assert_response :success
+  end
+
+end
